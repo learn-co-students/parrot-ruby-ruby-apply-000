37d9895865514afec2cc27b05a26474b8445294e
Starsky Eustache <star.eu88@gmail.com>
2016-08-14 23:43:15 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,15 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(string1 = "Squawk!",string2 = nil )
+    results = puts "#{string1}#{string2}"
+    if string1 == nil and string2 == nil
+        return string1
+    end
+    if string1 == nil
+        return string1
+    end
+    if string2 == nil
+        return string1
+    end
+    return results
+end
