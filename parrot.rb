f864a6ea9e73907214ba630f9163eeb33db8694a
Kira Rice <kiramrice@gmail.com>
2016-12-21 22:46:58 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,12 @@
 
-def parrot(phrase = "Squawk!")
-  puts "#{phrase}"
-  phrase
+def parrot(phrase = "Squawk!") #"Squawk!" becomes a default_argument, not a required argument
+  puts "#{phrase}" #will print "Squawk!"
+  phrase #will return "Squawk!"
 end
 
-parrot
-
-phrase = "Pretty bird!"
+phrase = "Pretty bird!" #same principle as above only redefined the default_argument
 parrot(phrase)
+
+#"In Ruby you can supply a default value for an argument. This means that if a value for the argument isn’t supplied, the default value will be used instead" -www.skorks.com
+#so phrase is the default_argument and 'Squawk!" is the value?
+#if method 'phrase' is invoked without calling any arguments, will assume value of method is "Squawk!"
