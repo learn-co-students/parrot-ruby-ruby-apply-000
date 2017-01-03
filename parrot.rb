f25b53bd5adcc8fda3343e585b07b1f970f7dce2
Ben Jay <fakebenjay@gmail.com>
2017-01-03 07:15:45 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(phrase = "Squawk!")
+  puts phrase
+  if phrase == nil || phrase == "Squawk!"
+    return "Squawk!"
+  else
+    ##Used an else statement here so that most variables for "phrase" would return "Pretty bird!"
+    ##If I had used an elsif statement here, most variables for "phrase" would return nil
+    ##I wanted this to be a very responsive and annoying parrot
+    return "Pretty bird!"
+  end
+end
