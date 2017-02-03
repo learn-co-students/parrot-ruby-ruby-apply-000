ec9bcf5f835f2d1e09575d4e9756e803d78ee60f
Ethan Ryan <ethanryan@gmail.com>
2017-02-03 17:40:57 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(phrase = "Squawk!") ##phrase is the argument, the default phrase = "Squawk!"
+  puts "#{phrase}" ## phrase is output as a string... if it isn't the default phrase, the phrase argument value will be output
+  return phrase ## phrase is returned... if it isn't the default phrase, the phrase argument value will be returned
+end
