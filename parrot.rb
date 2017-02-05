e4c3c45a6f99a74d74ba7229b2902e276b50ca0d
Brett Schules <brettschules@gmail.com>
2017-02-05 16:14:59 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phase = nil)
+  if phase == nil
+    puts "Squawk!"
+    return "Squawk!"
+  else
+    puts "#{phase}"
+    return "#{phase}"
+  end
+end
