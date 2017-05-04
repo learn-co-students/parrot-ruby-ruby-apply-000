88478b73aa7257189c3362a39666770f11b52f9b
bigxshowx <bigxshowx@gmail.com>
2017-05-04 18:03:29 +0900

    parrot completed

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(default=false)
+	if default == false
+		puts "Squawk!"
+		return 'Squawk!'
+	else
+		puts default
+		return default
+	end
+end
