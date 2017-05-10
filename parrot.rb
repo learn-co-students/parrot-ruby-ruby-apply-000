ecea3c5775b06789711377c45ade7e4a092f548e
Yasemin  Yurekli <yasemin@yureklis.com>
2017-05-10 18:16:43 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot( name= "My name is Yaso")
+  puts "#{name}"
+  name
+end
+
+def parrot (squawk = "Squawk!")
+puts "#{squawk}"
+squawk
+end
