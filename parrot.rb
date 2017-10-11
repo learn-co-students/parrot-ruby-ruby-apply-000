6d659b40199d4416948027d4986d2ee829b199e4
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-11 13:51:38 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,10 @@
-def parrot(*string)
-  if string == []
-    string = "Squawk!"
-    puts string
-    return string
-  end
-  string = string[0]
+def parrot(string = "Squawk!")
+  # if string == []
+  #   string = "Squawk!"
+  #   puts string
+  #   return string
+  # end
+  # string = string[0]
   puts string
   string
 end
