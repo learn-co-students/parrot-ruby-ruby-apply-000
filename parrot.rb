487e8f9165ddae4e4bd6a989c2f1820a79091d66
cjunks94 <cjunks94@gmail.com>
2018-03-13 20:43:31 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(str="Squawk!")
+  #newstr="Squawk!"
+
+    puts str
+    return str
+end
+
+
+#originally was trying to use optinial arguement. used if/else to check passed arg
+#left this idea after eating snacks and went with a simple def arg. prints the statement as 
+#Squawk (def value) unless an arg is passed.
