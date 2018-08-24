0465022014dde431af33ea379c5007b9d56cd2bc
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-08-24 05:24:24 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,13 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(string)
-  if string == ""
-    puts "Squawk!"
- else   
+def parrot(string="Squawk!")
+  
     puts string
   string
-end
+
 end
 
 parrot("")
