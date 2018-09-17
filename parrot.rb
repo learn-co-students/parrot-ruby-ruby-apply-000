5e08c8772af399d41392e31522bfe33ce03b7be7
RUBY STUDENT <m.umarowtm@gmail.com>
2018-09-17 03:12:31 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(*p)
-  puts "Squawk!"
-end 
-return parrot
+def parrot(say = "Squawk!")
+  puts say
+  return say
+end
