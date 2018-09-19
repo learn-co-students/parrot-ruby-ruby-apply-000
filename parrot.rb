d3473eca111d7d74cefc0a7c41b582523a2c651e
gentle-reference-3520 <ahill0592@gmail.com>
2018-09-19 15:20:58 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot(argument = "Squawk!")
+  phrase =  "#{argument}"
+  puts phrase
+  phrase
+end
+  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
