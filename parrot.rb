200ce93d6a7ea5a01d947ad0c1cc8cfa7ac65928
Simon Lee <simonlee0306@gmail.com>
2018-10-02 23:08:37 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
+def parrot(str)
+  str1 = str
+  if str1 == ""
+    str1 = "Squawk!"
+  end
+  put str
+  return str
+end
 # returns the phrase
