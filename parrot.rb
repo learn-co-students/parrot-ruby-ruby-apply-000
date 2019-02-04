89bd6927d1dd877123890b78d5db0f0e7e69e366
George Wong <gwong323@yahoo.com>
2019-02-04 21:52:53 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -2,7 +2,7 @@
 # returns the phrase
 
 
-def parrot(word = "Squawk!")
-puts word
-word
+def parrot(word = "Squawk!") #def oarrot method accepting word as argument with defaul of "Squawk!"
+puts word #display string entered or if none the default
+word #return the string at the end of the argument as return for puts command is always nil
 end
