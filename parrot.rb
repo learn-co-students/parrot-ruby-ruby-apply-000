37473b921315e71820c593a53d6e0710dd8c5cf6
astonishing-glitch-8485 <chaitanya.v.de@gmail.com>
2019-02-23 14:22:53 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(string)
+  puts "Squawk!"
+  puts
+  returns string
+end
+
+puts parrot("Hi my name is CHAG")
