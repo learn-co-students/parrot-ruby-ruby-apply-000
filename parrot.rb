d4a8b433a838215f17cebd7ecbc3249317310abd
darling-trace-1821 <kmanrique93@gmail.com>
2019-02-27 02:02:37 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
+#def parrot(str)
+ # str = "Squawk!"
+  #puts "#{str}"
+  #"#{str}"
+#end
+
+def parrot(string="Squawk!")
+ puts "#{string}"
+ "#{string}"
+end
