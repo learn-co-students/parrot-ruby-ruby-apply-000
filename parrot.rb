de0fa965b0b3be3e8c5e879295a02c1b214a5431
Aruni Ranaweera <arunixr@gmail.com>
2019-03-21 02:19:43 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(*phrase)
+  if phrase.empty?
+    puts "Squawk!"
+    "Squawk!"
+  else
+    out = phrase.join
+    puts out
+    out
+  end
+end
