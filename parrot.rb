47c01f7fe40d91b8dc99093b14fd857570e85321
Raphael Saphra <rsaphra@gmail.com>
2019-04-01 16:49:16 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot()
-  puts "Squawk!"
-  return "Squawk"
+def parrot(quote = "Squawk")
+  puts quote
+  return quote
 end
