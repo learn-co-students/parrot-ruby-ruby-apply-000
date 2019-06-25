21eb35638735575973e91ce015f4962127b95e38
Robert Eggen <rjegrad@yahoo.com>
2019-06-25 22:28:46 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def # create - an-array
+  [parrot]
+end
+def
+  def#add_element_to_end of an-array
+    [macau]
+  end
