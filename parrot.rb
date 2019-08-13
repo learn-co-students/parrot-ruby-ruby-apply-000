b97462857b2f5ada1bce70d47e89e18fd85ec951
Casey (Bevin) Curry <c.larsoncurry425@gmail.com>
2019-08-13 05:33:30 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(default_word ="Squawk!")
+  puts "#{default_word}"
+  "#{default_word}"
+end
+
+parrot
+parrot()
+parrot("Pretty bird!")
