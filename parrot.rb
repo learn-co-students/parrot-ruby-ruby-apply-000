c8834841684ff07aa349478a5525078ee6ceb902
reactive-channel-5568 <silkartem@gmail.com>
2020-01-24 19:21:28 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(string="Squawk!")
+  puts "#{string}"
+  "#{string}"
+end
+
+print parrot
+print parrot("Pretty bird!")
