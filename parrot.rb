a9472ce7b01fbf0d83f119b668af0ede7ef617bf
Benjamin Ko <benjamintko@gmail.com>
2020-03-16 02:03:06 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(default = "Squawk!")
+  puts default
+  default
+end
+
+
+# different return values of output vs. return (puts = nil vs. return = argument without puts or return)
