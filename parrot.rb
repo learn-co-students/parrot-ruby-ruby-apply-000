4e98909b5a51197b7401feac77113fc5fa12e76a
SunJet Liu <sunjetliu@gmail.com>
2020-08-15 17:52:31 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(stra="Squawk!")
+  puts stra
+  return stra
+end
+# def parrot(thi)
+# if
+#   puts "#{thi}"
+# else
+# puts "Squawk!"
+# end
+# "Squawk!"
+# end
