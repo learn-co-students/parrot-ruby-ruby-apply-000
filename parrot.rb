f064be59e054d1ca8847594e04fe1fb8af71b0ec
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-08-19 01:38:55 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string = "Squawk!")
-  puts string
-  string
+def parrot(phrase = "Squawk!")
+  puts phrase
+  return phrase
 end
